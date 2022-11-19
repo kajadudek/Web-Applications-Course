6a7542a92a05a315becc9e07d6b45da7aeb49a7f
Kaja <kajadudek02@gmail.com>
2022-11-19 17:22:40 +0100

    working zombie spawner

--- lab 3/zad12/script.js
+++ lab 3/zad12/script.js
@@ -1,33 +1,30 @@
 var howManyZombies = 0;
 var points = 0000;
+var zombieNumber = 0;
+var speed = 2;
+const game = document.getElementById("game");
 
 //Cursor move
-// const cursor = document.getElementById("cursor");
+const cursor = document.getElementById("cursor");
 
-// const moveCursor = (e) => {
-//     const mouseX = e.clientX;
-//     const mouseY = e.clientY;
-
-//     cursor.style.transform = `translate3d(${mouseX-35}px, ${mouseY-35}px, 0)`;
-// }
-
-// window.addEventListener('mousemove', moveCursor)
+function moveCursor(e){
+    cursor.style.top = e.pageY - 35 + "px";
+    cursor.style.left = e.pageX - 35 + "px";
+}
+window.addEventListener('mousemove', moveCursor)
 
 
 //Points change
 function pointsChange(){
     var points4digits = "000" + points;
-    document.getElementById("score").textContent = points4digits.slice(-4,points4digits.length);
+    document.getElementById("score").textContent = points4digits.slice(-4, points4digits.length);
 }
 
-//Zombie walk
+//Zombie walk animation
 const zombie = document.getElementById("zombie");
 const zombieSteps = document.getElementById("zombie-steps")
 var zombieWidth = zombie.clientWidth,           //200px
     zombieHeight = zombie.clientHeight;         //312px
-
-//Zombie walk animation
-var speed = 2;
 var stepCounter = 0;
 
 function zombieWalkAnimation(){
@@ -52,6 +49,8 @@ console.log()
 var flag = false;
 
 function zombieWalk() {
+    console.log(zombie,'x');
+    // var id = document.getElementById(id);
     position = zombie.getBoundingClientRect();
     const func = setTimeout(function() {
         requestAnimationFrame(zombieWalk);
@@ -66,18 +65,48 @@ function zombieWalk() {
     if (flag){ clearTimeout(func)}
 }
 
+//Generating zombie
+function zombieSpawner(){
+    var speed = Math.round(Math.random()*5);
+    var posY = Math.round(Math.random()*200); //px
+    var size = (0.9 + (Math.round(Math.random()*40)/100)); 
+    console.log(posY, size)
+    var zombie2 = zombie.cloneNode(true);
+    zombie2.classList.add("zombie");
+    zombie2.setAttribute('no', zombieNumber);
+    zombie2.addEventListener('click', zombieKill);
+    zombie2.style.bottom += posY + "px";
+    zombie2.style.transform = "scale("+size+")";
+    game.appendChild(zombie2);
+
+    zombieNumber += 1;
+}
+
+zombieSpawner();
+zombieSpawner();
+
+
 // window.addEventListener('click',zombieWalk);
 zombieWalk();
 
 //Zombie kill
-zombie.addEventListener('click',function(event){
+function zombieKill(event){
     zombie.remove();
     points += 12;
     pointsChange();
-    // clearInterval(func);
+    console.log('killed');
     event.stopPropagation();
-    console.log('killed')
-})
+}
+
+zombie.addEventListener('click', zombieKill);
+// zombie.addEventListener('click',function(event){
+//     zombie.remove();
+//     points += 12;
+//     pointsChange();
+//     // clearInterval(func);
+//     // event.stopPropagation();
+//     console.log('killed')
+// })
 
 //Missing the shot
 window.addEventListener('click', function() {

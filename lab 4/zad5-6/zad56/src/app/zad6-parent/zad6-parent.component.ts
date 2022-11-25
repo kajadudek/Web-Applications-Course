6539befab6bf53aca860fdb24c62bac55af7a0b5
Kaja <kajadudek02@gmail.com>
2022-11-25 20:12:13 +0100

    card parent init

---
+++ lab 4/zad5-6/zad56/src/app/zad6-parent/zad6-parent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-zad6-parent',
+  templateUrl: './zad6-parent.component.html',
+  styleUrls: ['./zad6-parent.component.css']
+})
+export class Zad6ParentComponent {
+
+}

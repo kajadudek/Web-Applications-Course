772b161455211ed0ad7cb5491d65ed01f16de0bd
Kaja <kajadudek02@gmail.com>
2022-11-25 18:05:42 +0100

    zad5 and zad6 init

---
+++ lab 4/zad5-6/zad56/src/app/zad5/zad5.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import carsData from 'src/assets/cars.json';
+
+@Component({
+  selector: 'app-zad5',
+  templateUrl: './zad5.component.html',
+  styleUrls: ['./zad5.component.css']
+})
+export class Zad5Component{
+
+  brand!: string;
+  model!: string;
+  color!: string;
+  lastColor!: string;
+  carsData: any;
+
+  constructor() { 
+    this.carsData = carsData
+  }
+  
+  colorsSelected = false
+  brandsSelected = false
+  modelsSelected = false
+
+  brandIsSelected(){
+    this.brandsSelected = true
+    this.modelsSelected = false
+    this.colorsSelected = false
+  }
+
+  modelIsSelected(){
+    this.modelsSelected = true
+    this.colorsSelected = false
+  }
+
+  colorIsSelected(color: any){
+    this.lastColor = color
+    this.colorsSelected = true;
+  }
+}
